cb35f23efe79ec09ff6b5fd84fc356de7d5af3bf
“hiepphamhnl” <97078239+kelvinphamhnl@users.noreply.github.com>
2022-10-28 17:09:10 +0700

    Edit link doc

--- src/constant.tsx
+++ src/constant.tsx
@@ -1,2 +1,2 @@
-export const DOCS_URL = 'https://docs.algobet-sports.com/'
+export const DOCS_URL = 'https://algobets-organization.gitbook.io/algobet/'
 export const PITCH_DECK_V1_URL = 'https://algobet-docs.s3.ap-southeast-1.amazonaws.com/Algobet-Pitch-Desk-v1.1.pdf'
