cbbfd8e3e65176ce14d8076e1b928af0865484a5
phongnadi <phongtran141297@gmail.com>
2022-11-20 13:27:21 +0700

    fix count

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -4,28 +4,28 @@ import $ from 'jquery'
 const speed = 1
 
 export default function Count({ unitScreen, endNumber }: { unitScreen: any; endNumber: number }) {
-  const [number, setNumber] = useState('0')
+  const [number, setNumber] = useState(endNumber)
 
-  const incNbrRec = useCallback(
-    (i: number) => {
-      if (i < endNumber) {
-        setNumber(`${i}`)
-        setTimeout(() => {
-          //Delay a bit before calling the function again.
-          incNbrRec(i + 1)
-        }, speed)
-      }
-      if (i === endNumber) {
-        setNumber(`${endNumber}+`)
-      }
-    },
-    [endNumber]
-  )
+  // const incNbrRec = useCallback(
+  //   (i: number) => {
+  //     if (i < endNumber) {
+  //       setNumber(`${i}`)
+  //       setTimeout(() => {
+  //         //Delay a bit before calling the function again.
+  //         incNbrRec(i + 50)
+  //       }, speed)
+  //     }
+  //     if (i === endNumber) {
+  //       setNumber(`${endNumber}+`)
+  //     }
+  //   },
+  //   [endNumber]
+  // )
 
-  useEffect(() => {
-    const i = 0
-    incNbrRec(i)
-  }, [incNbrRec])
+  // useEffect(() => {
+  //   const i = 0
+  //   incNbrRec(i)
+  // }, [incNbrRec])
 
   return number
 }
